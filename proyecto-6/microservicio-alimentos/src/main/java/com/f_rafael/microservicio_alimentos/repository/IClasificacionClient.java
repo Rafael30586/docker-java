0aa73a0480ba7449d618b1bfff8965d2f8042e82
Rafael305 <fraq86@gmail.com>
2026-05-13 16:19:41 -0300

    proyecto 6 realizado sin docker compose

--- proyecto-6/microservicio-alimentos/src/main/java/com/f_rafael/microservicio_alimentos/repository/IClasificacionClient.java
+++ proyecto-6/microservicio-alimentos/src/main/java/com/f_rafael/microservicio_alimentos/repository/IClasificacionClient.java
@@ -1,11 +1,12 @@
 package com.f_rafael.microservicio_alimentos.repository;
 
-import com.f_rafael.microservicio_alimentos.dto.ClasificacionDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservicio-clasificaciones", url = "http//:microservicio-clasificacion-p-6:8080")
+import com.f_rafael.microservicio_alimentos.dto.ClasificacionDto;
+
+@FeignClient(name = "microservicio-clasificaciones", url = "microservicio-clasificacion-p-6:8080")
 public interface IClasificacionClient {
 
     @GetMapping("/clasificaciones/{id}")
--- proyecto-6/microservicio-alimentos/src/main/java/com/f_rafael/microservicio_alimentos/repository/IMarcaClient.java
+++ proyecto-6/microservicio-alimentos/src/main/java/com/f_rafael/microservicio_alimentos/repository/IMarcaClient.java
@@ -1,11 +1,12 @@
 package com.f_rafael.microservicio_alimentos.repository;
 
-import com.f_rafael.microservicio_alimentos.dto.MarcaDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservicio-marcas", url = "http//:microservicio-marca-p-6:8080")
+import com.f_rafael.microservicio_alimentos.dto.MarcaDto;
+
+@FeignClient(name = "microservicio-marcas", url = "microservicio-marcas-p-6:8080")
 public interface IMarcaClient {
 
     @GetMapping("/marcas/{id}")
